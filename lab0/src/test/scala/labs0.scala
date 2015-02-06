5799f47f5d3f5cc7509f791a7d2727f1d975c9c0
Emil Lundberg <lundberg.emil@gmail.com>
2015-02-06 14:21:21 +0100

    Parameterize pascal test

--- lab0/src/test/scala/labs0.scala
+++ lab0/src/test/scala/labs0.scala
@@ -1,9 +1,45 @@
 import org.scalatest._
+import org.scalatest.prop.TableDrivenPropertyChecks._
 
-class Lab0Spec extends FlatSpec with Matchers {
+class Lab0Spec extends FunSpec with Matchers {
 
-  "The pascal function" should "return 1 for (0, 4)" in {
-    Main.pascal(0, 4) should be (1)
+  describe("The pascal function") {
+
+    val binomials = Table(
+
+        ("n", "k", "b"),
+        (  0,   0,   1),
+        (  0,   1,   1),
+        (  0,   4,   1),
+
+        (  1,   0,   1),
+        (  1,   1,   1),
+
+        (  2,   0,   1),
+        (  2,   1,   2),
+        (  2,   2,   1),
+
+        (  3,   0,   1),
+        (  3,   1,   3),
+        (  3,   2,   3),
+        (  3,   3,   1),
+
+        (  4,   0,   1),
+        (  4,   1,   4),
+        (  4,   2,   6),
+        (  4,   3,   4),
+        (  4,   4,   1),
+
+        (  0, 100,   1)
+
+    )
+
+    forAll(binomials) { (r: Int, c: Int, b: Int) =>
+      it(s"returns $b for ($c, $r)") {
+        Main.pascal(c, r) should be (b)
+      }
+    }
+
   }
 
 }
