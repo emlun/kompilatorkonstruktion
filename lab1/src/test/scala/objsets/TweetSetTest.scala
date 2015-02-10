77efba2e25b3485b30364a2c222317e38760e1a0
Emil Lundberg <lundberg.emil@gmail.com>
2015-02-10 15:17:23 +0100

    Add tests for filter method

--- lab1/src/test/scala/objsets/TweetSetTest.scala
+++ lab1/src/test/scala/objsets/TweetSetTest.scala
@@ -81,6 +81,63 @@ class TweetSetSpec extends FunSpec with Matchers {
 
     }
 
+    describe("has a filter method") {
+
+      it("which returns an empty set if the TweetSet is empty") {
+        val counter = new CallCounter
+        new Empty().filter((_) => true).foreach(counter)
+        counter.count should be (0)
+      }
+
+      it("which returns an empty set if the predicate accepts nothing") {
+        val counter = new CallCounter
+        val set = new Empty().incl(new Tweet(null, "foo", 0))
+
+        set.filter((_) => false).foreach(counter)
+
+        counter.count should be (0)
+      }
+
+      it("which returns the same set if the predicate accepts everything") {
+        val counter = new CallCounter
+        val tweet = new Tweet(null, "foo", 0)
+        val set = new Empty().incl(tweet)
+
+        val filtered = set.filter((_) => true)
+        filtered.foreach(counter)
+
+        counter.count should be (1)
+        filtered contains tweet should be (true)
+      }
+
+      it("which returns only those tweets that match the predicate") {
+        val goodTweets = Seq(
+          new Tweet(null, "msg1", 1),
+          new Tweet("user1", "msg2", 2),
+          new Tweet(null, "msg3", 3)
+        )
+        val badTweets = Seq(
+          new Tweet(null, "msg4", 0),
+          new Tweet("user2", "msg5", 0),
+          new Tweet(null, "msg6", 0)
+        )
+
+        var set: TweetSet = new Empty
+        goodTweets.foreach((tweet: Tweet) => { set = set.incl(tweet) })
+        badTweets. foreach((tweet: Tweet) => { set = set.incl(tweet) })
+
+        val filtered = set.filter(_.retweets > 0)
+
+        val counter = new CallCounter
+        filtered.foreach(counter)
+        counter.count should be (3)
+
+        goodTweets.foreach(filtered contains _ should be (true))
+        badTweets. foreach(filtered contains _ should be (false))
+      }
+
+    }
+
   }
 
 }
