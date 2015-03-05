d6cf240e858ac98f85751fff526b512a57818d4f
Emil Lundberg <lundberg.emil@gmail.com>
2015-03-05 15:12:10 +0100

    Add custom matcher for testing lexer output

--- lab2/src/test/scala/koolc/lexer/SourceLexerSpec.scala
+++ lab2/src/test/scala/koolc/lexer/SourceLexerSpec.scala
@@ -5,37 +5,51 @@ import scala.io.Source
 
 import org.scalatest.FunSpec
 import org.scalatest.Matchers
+import org.scalatest._
+import matchers._
 
 import utils._
-
 
 object StringToSource extends Pipeline[String, Source] {
   override def run(ctx : Context)(v : String): Source = Source.fromString(v)
 }
 
-class SourceLexerSpec extends FunSpec with Matchers {
+trait TokenMatchers {
 
-  implicit class TokenSeqExpector(val tokenSeq : Seq[TokenKind])
-    extends Pipeline[Iterator[Token], Boolean] {
+  class TokenIteratorMatchesExpectedTokensMatcher(expectedTokens: Seq[TokenKind])
+      extends Matcher[Iterator[Token]] {
 
-    override def run(ctx : Context)(v : Iterator[Token]) : Boolean = {
-      tokenSeq.corresponds(v.toList)((k: TokenKind, t: Token) => {
-        println(s"Processing token $t")
-        t.kind == k
+    def apply(left: Iterator[Token]) = {
+      var accepted = true
+      var messages: Seq[String] = Nil
+
+      left.zipAll(expectedTokens.toIterator, new Token(Tokens.BAD), Tokens.BAD).foreach((pair) => {
+        val actual = pair._1
+        val expected = pair._2
+
+        if(actual.kind != expected) {
+          messages = messages :+ s"Mismatch: ${actual.kind} is not $expected"
+          accepted = false
+        }
       })
+      MatchResult( accepted, messages.mkString("\n"), "Success")
     }
   }
+
+  def beTokens(expectedTokens: Seq[TokenKind]) = new TokenIteratorMatchesExpectedTokensMatcher(expectedTokens)
+}
+
+class SourceLexerSpec extends FunSpec with Matchers with TokenMatchers {
 
   describe("The SourceLexer") {
     import Tokens._
 
-    val ctx = Context(new Reporter, None, null)
+    val ctx = Context(new koolc.utils.Reporter, None, null)
     val lexer = StringToSource andThen SourceLexer
 
-    it("does something") {
-      val source = """foo"""
-      val pipeline = lexer andThen (PRINTLN :: LPAREN :: STRLITKIND :: RPAREN :: EOF :: Nil)
-      pipeline.run(ctx)(source) should be (true)
+    it("lexes Hello World correctly") {
+      val source = """println("Hello, World!")"""
+      lexer.run(ctx)(source) should beTokens (PRINTLN :: LPAREN :: STRLITKIND :: RPAREN :: EOF :: Nil)
     }
   }
 
