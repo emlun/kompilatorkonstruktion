6ee52ea499184e8e0015e3e9d8ff4df5fe0a8210
Emil Lundberg <emlun@kth.se>
2015-03-05 17:43:31 +0100

    Add more lexing test cases

--- lab2/src/test/scala/koolc/lexer/SourceLexerSpec.scala
+++ lab2/src/test/scala/koolc/lexer/SourceLexerSpec.scala
@@ -56,6 +56,16 @@ class SourceLexerSpec extends FunSpec with Matchers with TokenMatchers {
       val source = """println ( "Hello, World!" ) """
       lexer.run(ctx)(source) should beTokens (PRINTLN :: LPAREN :: STRLITKIND :: RPAREN :: EOF :: Nil)
     }
+
+    it("lexes Hello World with faulty tokens correctly") {
+      val source = """println ( & "Hello, World!" ) """
+      lexer.run(ctx)(source) should beTokens (PRINTLN :: LPAREN :: BAD :: STRLITKIND :: RPAREN :: EOF :: Nil)
+    }
+
+    it("lexes int-identifier") {
+      val source = """111aaa"""
+      lexer.run(ctx)(source) should beTokens (INTLITKIND :: IDKIND :: EOF :: Nil)
+    }
   }
 
 }
