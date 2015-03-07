7600da1874fdd047dc60a50644982d44e5aca8af
Emil Lundberg <emlun@kth.se>
2015-03-07 02:50:13 +0100

    Add matcher for testing Token values as well as kinds

--- lab2/src/test/scala/koolc/lexer/SourceLexerSpec.scala
+++ lab2/src/test/scala/koolc/lexer/SourceLexerSpec.scala
@@ -12,7 +12,7 @@ import utils._
 
 trait TokenMatchers {
 
-  class TokenIteratorMatchesExpectedTokensMatcher(val expected: Seq[TokenKind])
+  class TokenIteratorMatchesTokenKindsMatcher(val expected: Seq[TokenKind])
       extends Matcher[Iterator[Token]] {
 
     def apply(left: Iterator[Token]) = {
@@ -40,7 +40,41 @@ trait TokenMatchers {
     }
   }
 
-  def beTokens(expectedTokens: Seq[TokenKind]) = new TokenIteratorMatchesExpectedTokensMatcher(expectedTokens)
+  class TokenIteratorMatchesTokensMatcher(val expected: Seq[Token])
+      extends Matcher[Iterator[Token]] {
+
+    def apply(left: Iterator[Token]) = {
+      val leftSeq = left.toSeq
+      val pairs = leftSeq.zip(expected) map (pair => {
+        val (actual, expected) = pair
+        if(actual.kind == expected.kind) {
+          import Tokens.Value
+          if(Value(actual) == Value(expected))
+            (true, s"Ok: $actual is $expected")
+          else
+            (false, s"Value mismatch: $actual is not $expected")
+        } else {
+          (false, s"Kind mismatch: $actual is not $expected")
+        }
+      })
+
+      val messages = pairs map (_._2)
+
+      val (sameLength, lengthDiffMessages) =
+        if(leftSeq.size > expected.size)
+          (false, leftSeq drop expected.size map (t => s"Unexpected token found: $t"))
+        else if(leftSeq.size < expected.size)
+          (false, expected drop leftSeq.size map (t => s"Expected token kind not found: $t"))
+        else
+          (true, Nil)
+
+      val message = (messages ++ lengthDiffMessages) mkString "\n"
+      MatchResult(sameLength && (pairs forall (_._1)), message, message)
+    }
+  }
+
+  def beKinds(expectedTokens: Seq[TokenKind]) = new TokenIteratorMatchesTokenKindsMatcher(expectedTokens)
+  def beTokens(expectedTokens: Seq[Token]) = new TokenIteratorMatchesTokensMatcher(expectedTokens)
 }
 
 class SourceLexerSpec extends FunSpec with Matchers with TokenMatchers {
@@ -57,22 +91,22 @@ class SourceLexerSpec extends FunSpec with Matchers with TokenMatchers {
 
     it("lexes Hello World correctly") {
       val source = """println("Hello, World!")"""
-      lexed(source) should beTokens (PRINTLN :: LPAREN :: STRLITKIND :: RPAREN :: EOF :: Nil)
+      lexed(source) should beKinds (PRINTLN :: LPAREN :: STRLITKIND :: RPAREN :: EOF :: Nil)
     }
 
     it("lexes Hello World with whitespace correctly") {
       val source = """println ( "Hello, World!" ) """
-      lexed(source) should beTokens (PRINTLN :: LPAREN :: STRLITKIND :: RPAREN :: EOF :: Nil)
+      lexed(source) should beKinds (PRINTLN :: LPAREN :: STRLITKIND :: RPAREN :: EOF :: Nil)
     }
 
     it("lexes Hello World with faulty tokens correctly") {
       val source = """println ( & "Hello, World!" ) """
-      lexed(source) should beTokens (PRINTLN :: LPAREN :: BAD :: STRLITKIND :: RPAREN :: EOF :: Nil)
+      lexed(source) should beKinds (PRINTLN :: LPAREN :: BAD :: STRLITKIND :: RPAREN :: EOF :: Nil)
     }
 
     it("lexes int-identifier") {
       val source = """111aaa"""
-      lexed(source) should beTokens (INTLITKIND :: IDKIND :: EOF :: Nil)
+      lexed(source) should beKinds (INTLITKIND :: IDKIND :: EOF :: Nil)
     }
 
     it("ignores comments") {
@@ -83,7 +117,7 @@ class SourceLexerSpec extends FunSpec with Matchers with TokenMatchers {
         * // /*
         */pa(
       """
-      lexed(source) should beTokens (PRINTLN :: IDKIND :: IDKIND :: LPAREN :: EOF :: Nil)
+      lexed(source) should beKinds (PRINTLN :: IDKIND :: IDKIND :: LPAREN :: EOF :: Nil)
     }
 
     it("does not support nested block comments") {
@@ -94,7 +128,7 @@ class SourceLexerSpec extends FunSpec with Matchers with TokenMatchers {
         * // /*
         */pa(
       """
-      lexed(source) should beTokens (
+      lexed(source) should beKinds (
         PRINTLN ::
         IDKIND ::
         MINUS :: IDKIND :: THIS :: IDKIND ::
