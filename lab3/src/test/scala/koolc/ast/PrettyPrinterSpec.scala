6b45f7b8153d35a016f9cb11c251bc17bed305d9
Emil Lundberg <emlun@kth.se>
2015-03-24 00:10:49 +0100

    Test that pretty-printer outputs the same thing if run on its own output

--- lab3/src/test/scala/koolc/ast/PrettyPrinterSpec.scala
+++ lab3/src/test/scala/koolc/ast/PrettyPrinterSpec.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 import org.scalatest.FunSpec
 import org.scalatest.Matchers
 
-import lexer.Lexer
+import lexer._
 import utils._
 import Trees._
 
@@ -49,6 +49,17 @@ class PrettyPrinterSpec extends FunSpec with Matchers {
       val pipeline = Lexer andThen Parser andThen PrinterPipeline
       val output = pipeline.run(Context(reporter = new Reporter, outDir = None, file = Some(input)))(input)
       output should be (expected)
+    }
+
+    it("outputs the same thing if run on its own output.") {
+      val input = new File(getClass.getResource("/greeter.kool").toURI())
+      val firstPipeline = Lexer andThen Parser andThen PrinterPipeline
+      val secondPipeline = SourceLexer andThen Parser andThen PrinterPipeline
+
+      val firstOutput = firstPipeline.run(Context(new Reporter, None, Some(input)))(input)
+      val secondOutput = secondPipeline.run(Context(new Reporter, None, None))(Source fromString firstOutput)
+
+      secondOutput should be (firstOutput)
     }
 
   }
