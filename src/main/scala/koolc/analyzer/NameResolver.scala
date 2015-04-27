emlun/kompilatorkonstruktion

commit f85138da6d0a18e33965a797ff7cb9afdab9aca1

Revert "Replace method Symbolic.getSymbol: S with symbol: Option[S]"
This reverts commit 305dd6e60e193180b3c8d507ef250a34ac87b191.

src/main/scala/koolc/analyzer/Symbols.scala
@@ -20,7 +20,10 @@ object Symbols {
       this
     }
 
-    def symbol: Option[S] = _sym
+    def getSymbol: S = _sym match {
+      case Some(s) => s
+      case None => sys.error("Accessing undefined symbol.")
+    }
   }
 
   sealed abstract class Symbol extends Positioned {

commit d1d96f4e15d6fe970fc45e7d9d478f066dbb8bdc

Clean up compilation and runtime errors caused by f85138d

src/main/scala/koolc/analyzer/NameResolver.scala
@@ -34,11 +34,9 @@ object NameResolver {
         methodSym => "Method " + clazz.name + "." + methodSym.name
       } getOrElse { "Class " + clazz.name }
 
-      varOrParam.symbol map { symbol =>
-        if(!(used contains symbol)) {
-          ctx.reporter.warning(s"${kind} ${symbol.name} in ${ancestors} is never used.", symbol)
-        }
-      } orElse sys.error(s"${kind} has no symbol: ${varOrParam}")
+      if(!(used contains varOrParam.symbol)) {
+        ctx.reporter.warning(s"${kind} ${varOrParam.symbol.name} in ${ancestors} is never used.", varOrParam.symbol)
+      }
     }
 
     def lookupType(global: GlobalScope, mainSymbol: ClassSymbol)(id: Identifier): Option[ClassSymbol] =
@@ -146,7 +144,7 @@ object NameResolver {
         val usedVars = (method.stats flatMap setOnStatement _) ++:
           setOnExpression(method.retExpr)
 
-        method.args ++ method.vars foreach warnIfUnused(usedVars, clazz, method.symbol)
+        method.args ++ method.vars foreach warnIfUnused(usedVars, clazz, Some(method.symbol))
         usedVars
       }
 
@@ -188,12 +186,7 @@ object NameResolver {
       classDecl.vars foreach setSymbolReferences(lookupType, clazz, clazz.lookupVar _)
 
       val usedVars = classDecl.methods flatMap { method =>
-        method.symbol map { methodSymbol =>
-          setSymbolReferences(lookupType, clazz, methodSymbol.lookupVar _)(method)
-        } orElse {
-          sys.error(s"Method no longer has a symbol: ${method}")
-          None
-        } getOrElse Set.empty
+        setSymbolReferences(lookupType, clazz, method.symbol.lookupVar _)(method)
       }
       classDecl.vars foreach warnIfUnused(usedVars.toSet, clazz)
     }
@@ -204,10 +197,7 @@ object NameResolver {
     program.main.stats foreach setSymbolReferences(lookupType(global, mainSymbol), mainSymbol, (_ => None))
 
     program.classes foreach { clazz =>
-      clazz.symbol orElse {
-        sys.error(s"Class no longer has a symbol: ${clazz}")
-        None
-      } map setSymbolReferencesOnClass(lookupType(global, mainSymbol), mainSymbol, clazz)
+      setSymbolReferencesOnClass(lookupType(global, mainSymbol), mainSymbol, clazz)(clazz.symbol)
     }
 
     def checkMap(classMap:Map[String,MethodSymbol]):Boolean ={
@@ -245,22 +235,16 @@ object NameResolver {
 
     classSymbols.foreach {checkMethods(_)}
 
-    def setTypeOnVarOrParam(tpe: TypeTree, symbol: Option[VariableSymbol]) = {
-      symbol map { symbol =>
-        tpe match {
-          case id: Identifier => lookupType(global, mainSymbol)(id) map { symbol.setType(_) }
-          case _ => {}
-        }
+    def setTypeOnVarOrParam(tpe: TypeTree, symbol: VariableSymbol) = tpe match {
+        case id: Identifier => lookupType(global, mainSymbol)(id) map { symbol.setType(_) }
+        case _ => {}
       }
-    }
 
     program.classes foreach { clazz =>
-      clazz.symbol map { classSymbol =>
-        clazz.vars foreach { varpar => setTypeOnVarOrParam(varpar.tpe, varpar.symbol) }
-        clazz.methods foreach { method =>
-          method.args foreach { varpar => setTypeOnVarOrParam(varpar.tpe, varpar.symbol) }
-          method.vars foreach { varpar => setTypeOnVarOrParam(varpar.tpe, varpar.symbol) }
-        }
+      clazz.vars foreach { varpar => setTypeOnVarOrParam(varpar.tpe, varpar.symbol) }
+      clazz.methods foreach { method =>
+        method.args foreach { varpar => setTypeOnVarOrParam(varpar.tpe, varpar.symbol) }
+        method.vars foreach { varpar => setTypeOnVarOrParam(varpar.tpe, varpar.symbol) }
       }
     }
 

src/main/scala/koolc/analyzer/Symbols.scala
@@ -20,10 +20,7 @@ object Symbols {
       this
     }
 
-    def getSymbol: S = _sym match {
-      case Some(s) => s
-      case None => sys.error("Accessing undefined symbol.")
-    }
+    def symbol: S = _sym getOrElse sys.error("Accessing undefined symbol.")
   }
 
   sealed abstract class Symbol extends Positioned {

src/main/scala/koolc/ast/Printer.scala
@@ -7,6 +7,8 @@ Petter Lundahl
 package koolc
 package ast
 
+import scala.util.Try
+
 import Trees._
 
 object Printer extends (Tree => String) {
@@ -104,7 +106,7 @@ object Printer extends (Tree => String) {
       case Not(expr)         => "!" + print(expr,ident+1)
 
       case id:This       => "this" + id.symbolComment
-      case id:Identifier => id.value + id.symbolComment
+      case id:Identifier => id.value + (Try(id.symbolComment) getOrElse "#??")
 
       case _ => " >>> " + t.getClass.getSimpleName + "<<<"
     }

src/main/scala/koolc/ast/Trees.scala
@@ -18,7 +18,7 @@ object Trees {
   def trimLinesFromRight(s: String): String = s.lines map { _.replaceFirst("\\s+$", "") } mkString "\n"
 
   sealed trait SymbolicTree[S <: Symbol] extends Tree with Symbolic[S] {
-    def symbolComment: String = symbol map { sym => s"#${sym.id}" } getOrElse "#??"
+    def symbolComment: String = s"#${symbol.id}"
   }
 
   case class Program(main: MainObject, classes: List[ClassDecl]) extends Tree

src/test/scala/koolc/analyzer/NameAnalysisSpec.scala
@@ -152,22 +152,19 @@ class NameAnalysisSpec extends FunSpec with Matchers with ReporterMatchers with 
   }
 
   def checkTypes(tree: Tree): Unit = {
-    def checkSymbolType(symbol: Option[Symbol]) = symbol match {
-        case Some(sym) => sym match {
-          case sym: ClassSymbol    => sym.tpe should be (TObject(sym))
-          case sym: MethodSymbol   => sym.tpe should be (TUntyped)
-          case sym: VariableSymbol => sym.tpeTree match {
-            case t: BooleanType  => sym.tpe should be (TBoolean)
-            case t: IntType      => sym.tpe should be (TInt)
-            case t: StringType   => sym.tpe should be (TString)
-            case t: IntArrayType => sym.tpe should be (TArray)
-            case id: Identifier  => id.symbol match {
-              case Some(classSym: ClassSymbol) => sym.tpe should be (TObject(classSym))
-              case _                           => fail("Expected Identifier symbol to be a ClassSymbol.")
-            }
+    def checkSymbolType(symbol: Symbol) = symbol match {
+        case sym: ClassSymbol    => sym.tpe should be (TObject(sym))
+        case sym: MethodSymbol   => sym.tpe should be (TUntyped)
+        case sym: VariableSymbol => sym.tpeTree match {
+          case t: BooleanType  => sym.tpe should be (TBoolean)
+          case t: IntType      => sym.tpe should be (TInt)
+          case t: StringType   => sym.tpe should be (TString)
+          case t: IntArrayType => sym.tpe should be (TArray)
+          case id: Identifier  => id.symbol match {
+            case classSym: ClassSymbol => sym.tpe should be (TObject(classSym))
+            case _                     => fail("Expected Identifier symbol to be a ClassSymbol.")
           }
         }
-        case None => fail("Expected symbol to be set.")
       }
 
     tree match {
@@ -437,7 +434,9 @@ class NameAnalysisSpec extends FunSpec with Matchers with ReporterMatchers with 
         val retExpr: ExprTree = program.get.classes.head.methods.head.retExpr
         retExpr match {
           case MethodCall(_, id, _) => {
-            id.symbol should be (None)
+            if(Try(id.symbol).isSuccess) {
+              fail("Expected method call to not have a symbol attached yet.")
+            }
           }
           case _ => fail("Expected baz return expression to be method call, was: " + retExpr)
         }
@@ -458,9 +457,9 @@ class NameAnalysisSpec extends FunSpec with Matchers with ReporterMatchers with 
 
         val fooClass = program.get.classes.head
         val unusedSymbols: Seq[Symbol] =
-          (fooClass.vars.init map { _.symbol.get }) :::
-          fooClass.methods.head.args.last.symbol.get ::
-          fooClass.methods.head.vars.last.symbol.get ::
+          (fooClass.vars.init map { _.symbol }) :::
+          fooClass.methods.head.args.last.symbol ::
+          fooClass.methods.head.vars.last.symbol ::
           Nil
         unusedSymbols foreach { warningSpy verify (*, _) }
       })

src/test/scala/koolc/analyzer/SymbolMatchers.scala
@@ -8,19 +8,9 @@ trait SymbolMatchers {
 
   class ShouldHaveSameSymbolMatcher(val right: Symbolic[_ <: Symbol]) extends Matcher[Symbolic[_ <: Symbol]] {
     override def apply(left: Symbolic[_ <: Symbol]) =
-      left.symbol map { leftSymbol =>
-        right.symbol map { rightSymbol =>
-          MatchResult(leftSymbol.id == rightSymbol.id,
-            s"Expected ${left} to have the same symbol (${leftSymbol.id}) as ${right} (${rightSymbol.id})",
-            s"${left} has the same symbol (${leftSymbol.id}) as ${right} (${rightSymbol.id})"
-          )
-        } getOrElse MatchResult(false,
-          s"Expected ${right} to have a symbol attached.",
-          s"${right} has a symbol attached."
-        )
-      } getOrElse MatchResult(false,
-        s"Expected ${left} to have a symbol attached.",
-        s"${left} has a symbol attached."
+      MatchResult(left.symbol.id == right.symbol.id,
+        s"Expected ${left} to have the same symbol (${left.symbol.id}) as ${right} (${right.symbol.id})",
+        s"${left} has the same symbol (${left.symbol.id}) as ${right} (${right.symbol.id})"
       )
   }
 