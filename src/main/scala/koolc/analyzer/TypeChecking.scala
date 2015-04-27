89d6d1e9aa6e3c1ccccc085b4adf7a78056b830f
plundahl <plundahl@laptop>
2015-04-27 17:52:49 +0200

    Methods tcExpr and tcStat is fully expanded for all cases

--- src/main/scala/koolc/analyzer/TypeChecking.scala
+++ src/main/scala/koolc/analyzer/TypeChecking.scala
@@ -30,6 +30,19 @@ object TypeChecking extends Pipeline[ Option[Program], Option[Program]] {
       }
     }
 
+    def tcTypeTree (tTree: TypeTree): Type = {
+      tTree match {
+        case IntArrayType() => TArray
+        case IntType() => TInt
+        case BooleanType() => TBoolean
+        case StringType() => TString
+        case id@Identifier(value) => {
+          id.symbol.tpe
+        }
+
+      }
+    }
+
     def tcExpr(expr: ExprTree, expected: Type*): Type = {
       // TODO: Compute type for each kind of expression
       val tpe: Type = expr match {
@@ -86,7 +99,6 @@ object TypeChecking extends Pipeline[ Option[Program], Option[Program]] {
         case call@MethodCall(obj, methId, args) => {
           val objType = tcExpr(obj,anyObject)
           resolveMethodCall(call) flatMap { methodSymbol =>
-            println(methId)
 
             if(args.size == methodSymbol.argList.size) {
               args zip methodSymbol.argList foreach { case (arg, argDef) => tcExpr(arg, argDef.tpe) }
@@ -104,15 +116,29 @@ object TypeChecking extends Pipeline[ Option[Program], Option[Program]] {
             TError
           }
         }
+        case IntLit(value)=> TInt
+        case StringLit(value)=> TString
 
+        case True() => TBoolean
+        case False() => TBoolean
         case id@Identifier(value) => {
           id.symbol.tpe
         }
 
+        case id@This() => {
+          id.symbol.tpe
+        }
+        case NewIntArray(size) => {
+          tcExpr(size,TInt)
+          TArray
+        }
         case New(obj) => {
           tcExpr(obj,anyObject)
           obj.symbol.tpe
         }
+
+        case Not(expr) => tcExpr(expr,TBoolean)
+
         case _ => {
           println("tcExpr not implemented >>> " + expr)
           TUntyped
@@ -133,21 +159,55 @@ object TypeChecking extends Pipeline[ Option[Program], Option[Program]] {
 
     def tcStat(stat: StatTree): Unit = {
       stat match {
+        case Block(stats) => {
+          stats foreach {tcStat(_)}
+        }
         case If(expr, thn, els) => {
           tcExpr(expr,TBoolean)
+          tcStat(thn)
+          els match {
+            case Some(statTree) => tcStat(statTree)
+            case None =>
+          }
         }
-
+        case While(expr, stat) => {
+          tcExpr(expr,TBoolean)
+          tcStat(stat)
+        }
+        case Println(expr) => {
+          tcExpr(expr,TString,TInt,TBoolean)
+        }
+        case Assign(id, expr) => {
+          tcExpr(expr, id.symbol.tpe)
+        }
+        case ArrayAssign(id, index, expr) => {
+          tcExpr(id,TArray)
+          tcExpr(index,TInt)
+          tcExpr(expr,TInt)
+        }
         case _ => println("tcStat not implemented >>> " + stat)
       }
     }
 
     prog match {
-      case Some(p) => p.main.stats foreach { stat => tcStat(stat)}
+      case Some(p) => {
+        p.main.stats foreach { stat => tcStat(stat)}
+        p.classes foreach {
+          clazz => clazz.methods foreach {
+            method => {
+              method.stats foreach {
+                //println(_)
+                tcStat(_)
+              }
+              tcExpr(method.retExpr, tcTypeTree(method.retType))
+            }
+          }
+        }
+      }
       case None => ???
 
     }
 
-    println("hej")
 
     prog
   }
