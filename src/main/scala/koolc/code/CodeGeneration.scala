bf6207181b3f73ac8a70514c697d7de3eb6005b9
plundahl <plundahl@laptop>
2015-04-28 17:19:18 +0200

    First draft for generating java bytecode using cafebabe

--- src/main/scala/koolc/code/CodeGeneration.scala
+++ src/main/scala/koolc/code/CodeGeneration.scala
@@ -14,10 +14,46 @@ object CodeGeneration extends Pipeline[ Option[Program], Unit] {
   def run(ctx: Context)(prog: Option[Program]): Unit = {
     import ctx.reporter._
 
+    def typeToString(tpe: Type): String = {
+      tpe match{
+        case TString => "Ljava/lang/String;"
+        case TArray => "[I"
+        case TInt => "I"
+        case TBoolean => "Z"
+        case id@TObject(_) => "L" + id.toString + ";"
+      }
+      //[warn] It would fail on the following inputs: TError, TUnresolved, TUntyped
+    }
+
     /** Writes the proper .class file in a given directory. An empty string for dir is equivalent to "./". */
     def generateClassFile(sourceName: String, ct: ClassDecl, dir: String): Unit = {
       // TODO: Create code handler, save to files ...
-      ???
+
+      //Setting up the classfile
+      val classFile = new ClassFile(ct.id.value, None)
+      classFile.setSourceFile(sourceName)
+      classFile.addDefaultConstructor
+
+      //Adding fields
+      ct.vars foreach { field =>
+        val typeString = typeToString(field.symbol.tpe)
+
+        val fh: FieldHandler = classFile.addField(typeString, field.id.value)
+      }
+
+      //Adding methods:
+      ct.methods foreach { method =>
+        val rType = typeToString(method.retType.getType)
+        var parType = ""
+        method.args foreach { arg =>
+          parType += typeToString(arg.id.symbol.tpe)
+        }
+        val mh: MethodHandler = classFile.addMethod(rType, method.id.value, parType)
+        val ch: CodeHandler = mh.codeHandler
+        generateMethodCode(ch, method)
+      }
+
+      classFile.writeToFile("./"+ct.id.value+".class")
     }
 
     // a mapping from variable symbols to positions in the local variables
@@ -27,7 +63,7 @@ object CodeGeneration extends Pipeline[ Option[Program], Unit] {
 
       // TODO: Emit code
 
-      ch.freeze
+      //ch.freeze
     }
 
     def generateMainMethodCode(ch: CodeHandler, stmts: List[StatTree], cname: String): Unit = {
