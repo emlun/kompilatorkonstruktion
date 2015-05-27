50032e06482609c4e343e20be4a40a20de060074
Emil Lundberg <emlun@kth.se>
2015-05-27 23:16:36 +0200

    Add unit test for template expansion

---
+++ src/test/scala/koolc/template/TemplateExpansionSpec.scala
@@ -0,0 +1,66 @@
+package koolc
+package template
+
+import org.scalatest.FunSpec
+import org.scalatest.Inside
+import org.scalatest.Matchers
+
+import utils._
+import lexer._
+import ast._
+import analyzer._
+
+import Trees._
+
+class TemplateExpansionSpec extends FunSpec with TestUtils with Matchers with ReporterMatchers with Inside {
+
+  override def pipeline = Parser andThen ClassTemplateExpander andThen NameAnalysis andThen TypeChecking
+
+  describe("The compiler") {
+    it("expands method templates.") {
+      val source = """
+        object Main { def main(): Unit = { } }
+
+        class Bar<T> {}
+        class Foo {
+          def foo(): Foo = {
+            var a: Foo;
+            var b: Bar<Foo>;
+            var c: Bar<Bar<Foo>>;
+            a = this.bar<Foo>(new Foo());
+            b = this.bar<Bar<Foo>>(new Bar<Foo>());
+            c = this.bar<Bar<Bar<Foo>>>(new Bar<Bar<Foo>>());
+            return this.bar<Foo>(a);
+          }
+          def bar<T>(a: T): T = {
+            var b: T;
+            return new T();
+          }
+        }
+      """
+
+      checkResultForString(source) { (ctx, program) =>
+        ctx.reporter shouldBe errorless
+        program should not be None
+
+        inside(program.get.classes.last) { case clazz =>
+          clazz.methods.size should be (4)
+          inside(clazz.methods) { case List(methBarBarBarFoo, methBarBarFoo, methBarFoo, methFoo) =>
+            methFoo.id.name should be ("foo")
+
+            (methBarFoo, "bar$Foo", "Foo") ::
+            (methBarBarFoo, "bar$Bar$Foo", "Bar$Foo") ::
+            (methBarBarBarFoo, "bar$Bar$Bar$Foo", "Bar$Bar$Foo") ::
+            Nil foreach { case (meth, expectedName, expectedType) =>
+              meth.id.name should be (expectedName)
+              meth.retType should be (Identifier(expectedType, Nil))
+              meth.retExpr should be (New(Identifier(expectedType, Nil)))
+              meth.args.head should be (Formal(Identifier(expectedType, Nil), Identifier("a", Nil)))
+              meth.vars.head should be (VarDecl(Identifier(expectedType, Nil), Identifier("b", Nil)))
+            }
+          }
+        }
+      }
+    }
+  }
+}
